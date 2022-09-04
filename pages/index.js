193502092dfde7746f6027d540b386b40d5f3ba1
Tim Myers <tim@denvercoder.com>
2022-09-04 14:16:43 -0600

    remove footer

--- pages/index.js
+++ pages/index.js
@@ -23,9 +23,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            Powered by DenverCoder
           </span>
         </a>
       </footer>
